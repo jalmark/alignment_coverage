7da555e52126e2772b02e051cd55158e838f007e
Jalmari Kettunen <93248793+jalmark@users.noreply.github.com>
2021-12-15 19:52:09 +0200

    core functionality works

--- reads.py
+++ reads.py
@@ -2,27 +2,26 @@
 import sys
 
 def main():
+	reads = [0] * int(sys.argv[2])
 
-    reads = [0] * int(sys.argv[2])
+	data = open(sys.argv[1], "r")
 
-    data = open(sys.argv[1]) 
+	for i in data:
 
-    for i in data:
+        	row = i.split("\t")
 
-        row = i.split("\t")
+        	start = int(row[3])
+        	end = start + int(len(row[9]))
+        	#print("Start is %d  and end is %d" %(start,end))
 
-        start = int(row[3])
-        end = int(row[7])
-
-        if(start < end):
-            for j in range(start,end+1):
-                reads[j] = reads[j]+1
-
-
-    with open("data_out.txt", "w") as fileOut:
-        for item in reads:
-            fileOut.write("%d\n" %item)
+        	#if start < end:
+        	for j in range(start,end+1):
+            		reads[j] = reads[j]+1
 
 
+	with open("data_out.txt", "w") as fileOut:
+		for item in reads:
+			fileOut.write("%d\n" %item)
 
 main()
+
